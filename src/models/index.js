9d8f33aa4fe0efd2d76e2bf5304b7e8dbadd3e9c
Neel <ezebarazarte@gmail.com>
2023-06-11 16:33:38 -0400

    api lista

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Movie = require("./Movie")
+const Actor = require("./Actor")
+const Director = require("./Director")
+const Genre = require("./Genre")
+
+Movie.belongsToMany(Actor, { through: 'MoviesActors' });
+Actor.belongsToMany(Movie, { through: 'MoviesActors' });
+
+Movie.belongsToMany(Director, { through: 'MoviesDirectors' });
+Director.belongsToMany(Movie, { through: 'MoviesDirectors' });
+
+Movie.belongsToMany(Genre, { through: 'MoviesGenres' });
+Genre.belongsToMany(Movie, { through: 'MoviesGenres' });
